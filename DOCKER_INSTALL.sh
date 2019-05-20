32fa45315574a469485c8708d83a61187b6a0b66
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-05-20 14:04:17 +0200

    Update DOCKER_INSTALL.sh

--- DOCKER_INSTALL.sh
+++ DOCKER_INSTALL.sh
@@ -8,3 +8,18 @@ sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
+
+# CENT OS
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo usermod -aG docker $(whoami)
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+# DOCKER-COMPOSE
+sudo yum install epel-release
+sudo yum install -y python-pip
+sudo pip install docker-compose
+sudo yum upgrade python*
+docker-compose version
