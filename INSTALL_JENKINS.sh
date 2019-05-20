52708292e70fa4e62fd8d55237ef26132362dbeb
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-05-20 13:59:10 +0200

    Update INSTALL_JENKINS.sh

--- INSTALL_JENKINS.sh
+++ INSTALL_JENKINS.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# JENKINS
+sudo curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+sudo systemctl start jenkins
+systemctl status jenkins
+sudo systemctl enable jenkins
+
+# FIREWALL
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+sudo firewall-cmd --reload
+
+# SUIVRE INDICATIONS
