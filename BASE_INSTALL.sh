52b0a45ca7b0fbb994dbb96a456549da7d7e4996
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-05-20 19:28:55 +0200

    Create BASE_INSTALL.sh

---
+++ BASE_INSTALL.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# yum update
+sudo yum -y update
+
+# GIT
+sudo yum install git
+
+# JAVA
+sudo wget https://download.java.net/java/GA/jdk12/GPL/openjdk-12_linux-x64_bin.tar.gz
+sudo tar xzvf openjdk-12_linux-x64_bin.tar.gz
+sudo mv jdk-12 /opt/java
+sudo touch /etc/profile.d/jdk.sh
+sudo vim /etc/profile.d/jdk.sh
+
+	JAVA_HOME=/opt/java
+	PATH=$PATH:$JAVA_HOME/bin
+	export PATH JAVA_HOME
+	
+source /etc/profile
+
+# JENKINS
+sudo curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+sudo systemctl start jenkins
+systemctl status jenkins
+sudo systemctl enable jenkins
+
+# 
