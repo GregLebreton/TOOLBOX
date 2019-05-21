e3976ab73b8e7ae6318fd9e983e4127e74900a64
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-05-21 11:24:45 +0200

    Update MINIKUBE_INSTALL.sh

--- MINIKUBE_INSTALL.sh
+++ MINIKUBE_INSTALL.sh
@@ -8,4 +8,4 @@ sudo cp minikube /usr/local/bin && rm minikube
 # virtualbox
 sudo yum install -y kernel-devel kernel-headers make patch gcc
 sudo wget https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
-sudo yum -y install VirtualBox-5.2
+sudo yum -y install VirtualBox-5.0
