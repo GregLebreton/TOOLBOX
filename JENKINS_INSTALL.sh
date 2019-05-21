a80fabbb133658c4d1d95e707994211e3c58396f
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-05-21 11:17:48 +0200

    Update JENKINS_INSTALL.sh

--- JENKINS_INSTALL.sh
+++ JENKINS_INSTALL.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # JENKINS
-sudo curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo yum install java-1.8.0-openjdk-devel
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install -y jenkins
 sudo systemctl start jenkins
