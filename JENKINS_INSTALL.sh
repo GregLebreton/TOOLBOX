65e34b6f8c9ac058e9aba3ff3861f62789e712b5
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-05-21 10:39:03 +0200

    Update JENKINS_INSTALL.sh

--- JENKINS_INSTALL.sh
+++ JENKINS_INSTALL.sh
@@ -3,7 +3,7 @@
 # JENKINS
 sudo curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins
+sudo yum install -y jenkins
 sudo systemctl start jenkins
 systemctl status jenkins
 sudo systemctl enable jenkins
