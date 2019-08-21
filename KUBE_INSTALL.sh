2f048b8b763252547b14407f931c577c0e477592
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-08-21 23:26:08 +0200

    Create KUBE_INSTALL.sh

---
+++ KUBE_INSTALL.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet kubeadm kubectl
+sudo reboot
+systemctl start docker && systemctl enable docker
+systemctl start kubelet && systemctl enable kubelet
+
+sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+systemctl restart kubelet
+
